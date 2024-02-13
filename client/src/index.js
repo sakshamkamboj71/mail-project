799a2466bb7310aca5b1d53c5832687b1c796a56
Saksham Kamboj <83581167+sakshamkamboj71@users.noreply.github.com>
2024-02-13 23:09:51 +0530

    added firebase config message

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDLI3SPU7tpY3ll_yVmQzExUov-5R09DEg",
-  authDomain: "mail-adf4c.firebaseapp.com",
-  projectId: "mail-adf4c",
-  storageBucket: "mail-adf4c.appspot.com",
-  messagingSenderId: "249780914377",
-  appId: "1:249780914377:web:acc3f7dd7c909987202518",
-};
+//Add your firebase config here.
 
 firebase.initializeApp(firebaseConfig);
 
